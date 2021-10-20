78f2a53f602c96bd9c3185c7747999c3b98f6e2d
LYshiying <839778960@qq.com>
2021-10-20 16:19:31 +0800

    :tada: 新增随机发病功能

--- src/plugins/zijiang/__init__.py
+++ src/plugins/zijiang/__init__.py
@@ -1,13 +1,15 @@
+import random
 import time
 from nonebot import CommandSession
 
-from .get_api import cachong
+from .get_api import cachong, haha
 from src.Services import uiPlugin
 
 sv_help = """枝江查重| 使用说明
 [小作文查重 (作文)] -> 送去枝江查重
     特别注意:
         字数要求: 10-1000
+[随机发病] -> 三天内前十条随机发病集合捏~
 """
 
 sv = uiPlugin(["zijiang", "枝江查重"], False, usage=sv_help)
@@ -27,9 +29,15 @@ async def yanbijinggao(session: CommandSession):
         text = (
             f"被发现了捏~\n"
             f"查重率:{int(res['data']['related'][0]['rate'])*100}%\n"
-            f"原创|原偷作者: {res['data']['related'][0]['reply']['m_name']}(uid: {res['data']['related'][0]['reply']['uid']})\n"
+            f"原创|原偷作者: {res['data']['related'][0]['reply']['m_name']}(uid: {res['data']['related'][0]['reply']['mid']})\n"
             f"原文: {res['data']['related'][0]['reply']['content']}\n"
             f"发送时间: {time.strftime('%Y年%m月%d日 %H:%M:%S',time.localtime(int(res['data']['related'][0]['reply']['ctime'])))}\n"
             f"原文链接: {res['data']['related'][0]['reply_url']}"
         )
         await session.finish(text)
+
+
+@sv.ui_command("随机发病")
+async def random_haha(session: CommandSession):
+    res = await haha()
+    await session.finish(random.choice(res))
--- src/plugins/zijiang/get_api.py
+++ src/plugins/zijiang/get_api.py
@@ -1,4 +1,5 @@
 import json
+import time
 
 from soraha_utils import async_uiclient, retry, logger
 import config
@@ -13,3 +14,15 @@ async def cachong(text):
     ) as client:
         res = await client.uipost("https://asoulcnki.asia/v1/api/check")
     return res.json()
+
+
+@retry(logger=logger)
+async def haha():
+    params = {"timeRangeMode": 2, "sortMode": 0, "pageNum": 1, "pageSize": 10}
+    async with async_uiclient(request_params=params) as client:
+        res = await client.uiget("https://asoulcnki.asia/v1/api/ranking/")
+    js = res.json()
+    return [
+        f"用户名: {i['m_name']}(uid: {i['mid']})\n评论: {i['content']}\n发布时间: {time.strftime('%Y年%m月%d日 %H:%M:%S',time.localtime(int(i['ctime'])))}"
+        for i in js["data"]["replies"]
+    ]
